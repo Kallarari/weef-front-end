63118cb73b80986e897e654cdad3166a35b426d6
Minosso <kallarari@gmail.com>
2022-07-14 05:20:07 -0300

    feat (): set environment

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.ts'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+import GlobalStyle from '../styles/globals';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+    <GlobalStyle /><Component {...pageProps} /></ThemeProvider>);
+}
+
+export default MyApp
